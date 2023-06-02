0d9975c256915dc7c556f4ad783f74d07e3eb06e
unknown <hitprofit70@gmail.com>
2023-06-02 15:03:12 +0200

    footer updated

--- frontend/src/components/BottomF.jsx
+++ frontend/src/components/BottomF.jsx
@@ -4,12 +4,13 @@ const BottomF = () => {
   return (
     <div>
       <footer className="footer">
-    <div className="waves">
-      <div className="wave" id="wave1"></div>
-      <div className="wave" id="wave2"></div>
-      <div className="wave" id="wave3"></div>
-      <div className="wave" id="wave4"></div>
-    </div>
+    <ul className="menu">
+      <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
+    </ul>
     <ul className="social-icon">
       <li className="social-icon__item"><a className="social-icon__link" href="#">
           <ion-icon name="logo-facebook"></ion-icon>
@@ -24,15 +25,13 @@ const BottomF = () => {
           <ion-icon name="logo-instagram"></ion-icon>
         </a></li>
     </ul>
-    <ul className="menu">
-      <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
-      <li className="menu__item"><a className="menu__link" href="#">About</a></li>
-      <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
-      <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
-      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
-
-    </ul>
     <p>&copy;2021 Nadine Coelho | All Rights Reserved</p>
+    <div className="waves">
+      <div className="wave" id="wave1"></div>
+      <div className="wave" id="wave2"></div>
+      <div className="wave" id="wave3"></div>
+      <div className="wave" id="wave4"></div>
+    </div>
   </footer>
     </div>
   )

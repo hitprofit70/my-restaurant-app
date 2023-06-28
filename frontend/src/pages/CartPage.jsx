833c57c3a0d5a33217cf53b6dfb3ec1fe42b70f1
hitprofit70 <hitprofit70@gmail.com>
2023-06-28 16:13:18 +0200

    listgroup added

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -7,7 +7,13 @@ const CartPage = () => {
     <div>
     <TopNav/>
     <div className='container'>
-    <h1>WAHHS</h1>
+    <ul class="list-group list-group-horizontal">
+  <li class="list-group-item bg-black">
+  <img src='https://t3.ftcdn.net/jpg/05/81/94/08/240_F_581940851_vrw6QHGEaHbM5V8mRsafjfWQXW0o7pZt.jpg'/>
+  </li>
+  <li class="list-group-item">Meat</li>
+  <li class="list-group-item">R190</li>
+</ul>
     </div>
     <BottomF/>
     </div>

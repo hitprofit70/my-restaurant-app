9bd618a3d0a4cf69595ca9db73fa5e3d183d0796
hitprofit70 <hitprofit70@gmail.com>
2023-06-30 13:23:12 +0200

    fetch http added

--- frontend/src/pages/ViewProducts.jsx
+++ frontend/src/pages/ViewProducts.jsx
@@ -9,6 +9,7 @@ import { useParams } from 'react-router-dom';
 const ViewProducts = () => {
   const [myProducts, setMyProducts] = useState([]);
   const [myDrinks, setrMyDrinks] = useState([]);
+  const [myFoods, setMyFoods] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -21,6 +22,12 @@ const ViewProducts = () => {
     fetch(`http://localhost:3000/drinks/${id}`)
       .then((response) => response.json())
       .then((data) => setrMyDrinks(data.data))
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/foods/${id}`)
+    .then((response) => response.json())
+    .then((data) => setMyFoods(data.data))
   }, []);
 
   return (
@@ -68,6 +75,33 @@ const ViewProducts = () => {
                 <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
                   <h4 className="bg-dark mr-5 text-light">{myDrinks.name}</h4>
                   <h6 className="border bg-dark text-light">R{myDrinks.price}</h6>
+                  <span className="border bg-dark text-light">Free Delivery</span>
+                  <div className="">
+                    <button
+                      className="add-button mt-3 animate__heartBeat"
+                    >
+                      AddtoCart
+                    </button>
+                  </div>
+                </Stack>
+              </Col>
+            </Row>
+          </div>
+        )}
+        {myFoods && (
+          <div className="row">
+            <Row>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <img
+                  style={{ width: "100%", height: "300px" }}
+                  src={myFoods.imgP}
+                  alt="First slide"
+                />
+              </Col>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
+                  <h4 className="bg-dark mr-5 text-light">{myFoods.name}</h4>
+                  <h6 className="border bg-dark text-light">R{myFoods.price}</h6>
                   <span className="border bg-dark text-light">Free Delivery</span>
                   <div className="">
                     <button

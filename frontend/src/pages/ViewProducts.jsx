300613ac39e6e6f9cd139c347caecdbfd77d5b3c
hitprofit70 <hitprofit70@gmail.com>
2023-06-30 10:08:49 +0200

    findsingleproduct and params.id

--- frontend/src/pages/ViewProducts.jsx
+++ frontend/src/pages/ViewProducts.jsx
@@ -8,6 +8,7 @@ import { useParams } from 'react-router-dom';
 
 const ViewProducts = () => {
   const [myProducts, setMyProducts] = useState([]);
+  const [myDrinks, setrMyDrinks] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,36 +17,70 @@ const ViewProducts = () => {
       .then((data) => setMyProducts(data.data))
   }, []);
 
+  useEffect(() => {
+    fetch(`http://localhost:3000/drinks/${id}`)
+      .then((response) => response.json())
+      .then((data) => setrMyDrinks(data.data))
+  }, []);
+
   return (
     <div>
       <TopNav />
       <div className='container mt-5'>
-        <div className="row">
-          <Row>
-            <Col style={{ width: "350px", height: "350px" }}>
-              <img
-                style={{ width: "100%", height: "300px" }}
-                src={myProducts.imgP}
-                alt="First slide"
-              />
-            </Col>
-            <Col style={{ width: "350px", height: "350px" }}>
-              <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
-                <h4 className="bg-dark mr-5 text-light">{myProducts.name}</h4>
-                <h6 className="border bg-dark text-light">R{myProducts.price}</h6>
-                <span className="border bg-dark text-light">Free Delivery</span>
-                <div className="">
-                  <button
-                    className="add-button mt-3 animate__heartBeat"
-                  >
-                    AddtoCart
-                  </button>
-                </div>
-              </Stack>
-            </Col>
-          </Row>
-        </div>
-
+        {myProducts && (
+          <div className="row">
+            <Row>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <img
+                  style={{ width: "100%", height: "300px" }}
+                  src={myProducts.imgP}
+                  alt="First slide"
+                />
+              </Col>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
+                  <h4 className="bg-dark mr-5 text-light">{myProducts.name}</h4>
+                  <h6 className="border bg-dark text-light">R{myProducts.price}</h6>
+                  <span className="border bg-dark text-light">Free Delivery</span>
+                  <div className="">
+                    <button
+                      className="add-button mt-3 animate__heartBeat"
+                    >
+                      AddtoCart
+                    </button>
+                  </div>
+                </Stack>
+              </Col>
+            </Row>
+          </div>
+        )}
+        {myDrinks && (
+          <div className="row">
+            <Row>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <img
+                  style={{ width: "100%", height: "300px" }}
+                  src={myDrinks.imgP}
+                  alt="First slide"
+                />
+              </Col>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
+                  <h4 className="bg-dark mr-5 text-light">{myDrinks.name}</h4>
+                  <h6 className="border bg-dark text-light">R{myDrinks.price}</h6>
+                  <span className="border bg-dark text-light">Free Delivery</span>
+                  <div className="">
+                    <button
+                      className="add-button mt-3 animate__heartBeat"
+                    >
+                      AddtoCart
+                    </button>
+                  </div>
+                </Stack>
+              </Col>
+            </Row>
+          </div>
+        )}
       </div>
       <BotttomF />
     </div>

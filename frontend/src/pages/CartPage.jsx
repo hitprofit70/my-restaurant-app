c5cfb0defeb63215361f47dfb01420ed43a39518
hitprofit70 <hitprofit70@gmail.com>
2023-09-13 10:23:38 +0200

    card to be centered

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,8 +1,5 @@
 import TopNav from '../components/TopNav'
 import BottomF from '../components/BottomF'
-import Stack from "react-bootstrap/Stack";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 const CartPage = () => {
     return (
@@ -11,18 +8,17 @@ const CartPage = () => {
             <div className='container mt-5'>
                 <div className="items-group" style={{ height: "300px", width: "100%", borderRadius: "5px" }}>
                     <div className="container-fluid">
-                        <div className="col-9">
+                        <div className="col">
                             <img src='https://t3.ftcdn.net/jpg/05/81/94/08/240_F_581940851_vrw6QHGEaHbM5V8mRsafjfWQXW0o7pZt.jpg' className='mt-3' style={{ borderRadius: "5px" }} />
                         </div>
-                        <Row>
-                        <Col style={{ width: "300px", height: "350px" }}>
-                          <Stack className="text-center" gap={3}>
-                            <h4 className="bg-dark text-light" style={{borderRadius: "5px", width: "250px"}}>Steak With Leave</h4>
-                            <h6 className="border bg-dark text-light" style={{borderRadius: "5px", width: "250px"}}>R126</h6>
-                          </Stack>
-                        </Col>
-                      </Row>
-                    </div>        
+                        <div className=''>
+                            <span className="text-center">
+                                <h4 className="col item-name bg-dark text-light" style={{ borderRadius: "5px", width: "250px" }}>Steak With Leave</h4>
+                                <h6 className="col item-price border bg-dark text-light" style={{ borderRadius: "5px", width: "250px" }}>R126</h6>
+                            </span>
+                            <button className='checkout-btn'>CheckOut</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <BottomF />
